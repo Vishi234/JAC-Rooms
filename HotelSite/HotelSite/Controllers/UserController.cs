51c279ea294a85d0cdc80a6e21844051eff8d461
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2017-12-27 23:19:07 +0530

    Hotel Project

---
+++ HotelSite/HotelSite/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using HotelSite.Models.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelSite.Controllers
+{
+    [SessionState(System.Web.SessionState.SessionStateBehavior.Default)]
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [ActionName("Register")]
+        [HttpPost]
+        public ActionResult RegisterUser(Login lgnDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                UpdateModel(lgnDetail);
+                Register register = new Register();
+                int rslt = register.CreateUser(lgnDetail);
+                if (rslt > 1) return View("Login");
+                else return View();
+            }
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoggingIN(Signin signin)
+        {
+            //foreach(var frm in formCollection.AllKeys)
+            //{
+
+
+            //}
+
+            Register register = new Register();
+            if(register.SignIn(signin))
+            {
+                return View("Dashboard");
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ HotelSite/HotelSite/Models/Login/Login.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ApplicationBlocks.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Web;
+
+namespace HotelSite.Models.Login
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Signin
+    {
+        public string EmailID { get; set; }
+        public string Password { get; set; }
+
+    }
+    public class Register
+    {
+
+        string sqlconn = ConfigurationManager.ConnectionStrings["DBCONN"].ConnectionString;
+        public int CreateUser(Login lgn)
+        {
+            int result = 0;
+            try
+            {
+                //sp_Register_User
+                List<SqlParameter> lstsqlparam = new List<SqlParameter>();
+                lstsqlparam.Add(new SqlParameter("@Email", lgn.Email));
+                lstsqlparam.Add(new SqlParameter("@Mobile", lgn.Mobile));
+                lstsqlparam.Add(new SqlParameter("@Password", lgn.Password));
+                lstsqlparam.Add(new SqlParameter("@FirstName", lgn.FirstName));
+                lstsqlparam.Add(new SqlParameter("@LastName", lgn.LastName));
+                lstsqlparam.Add(new SqlParameter("@CreatedBy", "sreekumar"));
+                lstsqlparam.Add(new SqlParameter("@UpdatedBy", "sreekumar"));
+                int a = SqlHelper.ExecuteNonQuery(sqlconn, "sp_Register_User", lstsqlparam.ToArray());
+                result = a;
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+
+            }
+            return result;
+        }
+        public bool SignIn(Signin signin)
+        {
+            try
+            {
+                List<SqlParameter> lstsqlparam = new List<SqlParameter>();
+                lstsqlparam.Add(new SqlParameter("@EmailID", signin.EmailID));
+                lstsqlparam.Add(new SqlParameter("@Password", signin.Password));
+                DataSet ds = SqlHelper.ExecuteDataset(sqlconn, "sp_Login", lstsqlparam.ToArray());
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    HttpContext.Current.Session["UserID"] = ds.Tables[0].Columns["UserID"];
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
