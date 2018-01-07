878503f29fd656375a2551d891afb5bf7cc1b3ac
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2018-01-07 15:50:35 +0530

    Changes More

--- HotelSite/HotelSite/scripts/filter-tags.js
+++ HotelSite/HotelSite/scripts/filter-tags.js
@@ -1,51 +1,52 @@
-(function() {
+(function () {
+    var $base = $('#gallery div .base');
+    var $imgs = $('#gallery img');                  // Store all images
+    var $buttons = $('#buttons');                   // Store buttons element
+    var tagged = {};                                // Create tagged object
 
-  var $imgs = $('#gallery img');                  // Store all images
-  var $buttons = $('#buttons');                   // Store buttons element
-  var tagged = {};                                // Create tagged object
+    $base.each(function () {                         // Loop through images and
+        var img = this;                               // Store img in variable
+        var tags = $(this).data('tags');              // Get this element's tags
 
-  $imgs.each(function() {                         // Loop through images and
-    var img = this;                               // Store img in variable
-    var tags = $(this).data('tags');              // Get this element's tags
+        if (tags) {                                   // If the element had tags
+            tags.split(',').forEach(function (tagName) { // Split at comma and
 
-    if (tags) {                                   // If the element had tags
-      tags.split(',').forEach(function(tagName) { // Split at comma and
-
-        if (tagged[tagName] == null) {            // If object doesn't have tag
-          tagged[tagName] = [];                   // Add empty array to object
+                if (tagged[tagName] == null) {            // If object doesn't have tag
+                    tagged[tagName] = [];                   // Add empty array to object
+                }
+                tagged[tagName].push(img);                // Add the image to the array
+            });
         }
-        tagged[tagName].push(img);                // Add the image to the array
-      });
-    }
-  });
+    });
 
-  $('<button/>', {                                 // Create empty button
-    text: 'Show All',                              // Add text 'show all'
-    class: 'photoButton active',                               // Make it active
-    click: function() {                            // Add onclick handler to
-      $(this)                                      // Get the clicked on button
-        .addClass('active')                        // Add the class of active
-        .siblings()                                // Get its siblings
-        .removeClass('active');                    // Remove active from siblings
-      $imgs.hide().slideDown(500);                 // Show all images
-    }
-  }).appendTo($buttons);                           // Add to buttons
+    $('<button/>', {                                 // Create empty button
+        text: 'Show All',                              // Add text 'show all'
+        class: 'photoButton active',                               // Make it active
+        click: function () {                            // Add onclick handler to
+            $(this)                                      // Get the clicked on button
+              .addClass('active')                        // Add the class of active
+              .siblings()                                // Get its siblings
+              .removeClass('active');                    // Remove active from siblings
+            $base.hide().slideDown(500);
+            //$imgs.hide().slideDown(500);                 // Show all images
+        }
+    }).appendTo($buttons);                           // Add to buttons
 
-  $.each(tagged, function(tagName) {               // For each tag name
-    $('<button/>', {                               // Create empty button
-      text: tagName + ' (' + tagged[tagName].length + ')', // Add tag name
-      class:'photoButton',
-      click: function() {                          // Add click handler
-        $(this)                                    // The button clicked on
-          .addClass('active')                      // Make clicked item active
-          .siblings()                              // Get its siblings
-          .removeClass('active');                  // Remove active from siblings
-        $imgs                                      // With all of the images
-          .hide()                                  // Hide them
-          .filter(tagged[tagName])                 // Find ones with this tag
-          .fadeIn(500);                                // Show just those images
-      }
-    }).appendTo($buttons);                         // Add to the buttons
-  });
+    $.each(tagged, function (tagName) {               // For each tag name
+        $('<button/>', {                               // Create empty button
+            text: tagName + ' (' + tagged[tagName].length + ')', // Add tag name
+            class: 'photoButton',
+            click: function () {                          // Add click handler
+                $(this)                                    // The button clicked on
+                  .addClass('active')                      // Make clicked item active
+                  .siblings()                              // Get its siblings
+                  .removeClass('active');                  // Remove active from siblings
+                $base                                      // With all of the images
+                .hide()                                  // Hide them
+                .filter(tagged[tagName])                 // Find ones with this tag
+                .fadeIn(500);                                // Show just those images
+            }
+        }).appendTo($buttons);                         // Add to the buttons
+    });
 
 }());
