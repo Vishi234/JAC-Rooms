4bd84d800b397d19cf2866b5f02aa2bc6f205b6d
Sreekumar <esreekumar@outlook.com>
2018-01-07 23:46:19 +0530

    Agent Email verifiation

--- HotelSite/HotelSite/Controllers/AgentController.cs
+++ HotelSite/HotelSite/Controllers/AgentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelSite.Models.Common;
+using HotelSite.Models.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -24,6 +26,35 @@ namespace HotelSite.Controllers
         public ActionResult login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult RegisterAgent(FormCollection formCollection)
+        {
+            string Email = formCollection["EmailID"].ToString();
+            string Password = formCollection["Password"].ToString();
+            string HotelType = formCollection["HotelType"].ToString();
+            string FullName = formCollection["FullName"].ToString();
+            Agent agent = new Agent();
+            string Guid = agent.CreateAgent(Email, Password, HotelType, FullName);
+            string body = "Hello " + FullName + ",";
+            body += "<br /><br />Please click the following link to activate your account";
+            body += "<br /><a href = '" + string.Format("{0}://{1}/Agent/AgentComfirmation/{2}", Request.Url.Scheme, Request.Url.Authority, Guid) + "'>Click here to activate your account.</a>";
+            body += "<br /><br />Thanks";
+            Common.SendEmail(body, "clickhere");
+            return View("UserValidation");
+        }
+
+        public ViewResult AgentComfirmation()
+        {
+            Guid activationCode = new Guid(RouteData.Values["id"].ToString());
+            Agent agent = new Agent();
+            int rslt = agent.CheckAgent(activationCode);
+            if (rslt == 1) return View("Index");
+            else
+            {
+                ViewBag.Result = rslt;
+                return View("UserValidation");
+            }
         }
     }
 }
