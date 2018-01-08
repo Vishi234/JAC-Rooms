a09867fab12f50493d2ccf27bc4bf188823bef91
Sreekumar <esreekumar@outlook.com>
2018-01-08 06:45:34 +0530

    remove namespace

--- HotelSite/HotelSite/Controllers/AgentController.cs
+++ HotelSite/HotelSite/Controllers/AgentController.cs
@@ -1,9 +1,6 @@
 ﻿using HotelSite.Models.Common;
 using HotelSite.Models.Login;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace HotelSite.Controllers
