309697327b7a40564af1c1eddabef3f7fd80494a
Sreekumar <esreekumar@outlook.com>
2018-01-10 23:18:13 +0530

    Bundling

--- HotelSite/HotelSite/App_Start/BundleConfig.cs
+++ HotelSite/HotelSite/App_Start/BundleConfig.cs
@@ -10,8 +10,17 @@ namespace HotelSite
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundle/jquery").Include("~/scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/Content/css").IncludeDirectory("~/Content/", "*.css", true));
+            bundles.Add(new ScriptBundle("~/bundle/jquery").Include("~/scripts/jquery-1.4.2.min.js",
+                "~/scripts/jquery-1.8.5.min.js",
+                "~/scripts/jquery-1.11.2.min.js",
+                "~/scripts/jquery-1.9.1.min.js",
+                "~/scripts/jquery-3.2.1.min.js"));
+
+
+
+            bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory("~/Content/", "*.css", true));
+
+
             BundleTable.EnableOptimizations = true;
         }
     }
