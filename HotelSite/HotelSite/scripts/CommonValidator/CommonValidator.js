4c3b5d95de23c9bc22aaaf3ec7fa1cb177f60ed2
Sreekumar <esreekumar@outlook.com>
2018-01-26 14:14:39 +0530

    Validations rule

--- HotelSite/HotelSite/scripts/CommonValidator/CommonValidator.js
+++ HotelSite/HotelSite/scripts/CommonValidator/CommonValidator.js
@@ -1,5 +1,29 @@
-﻿let numberOnly = []
-$('[NumberOnly]').keydown(function (evt) {
+﻿$('[NumberOnly]').keydown(function (e) {
+    if (e.shiftKey || e.ctrlKey || e.altKey) {
+        e.preventDefault();
+    }
+    else {
+        var key = e.keyCode;
+        if (!((key == 8) || (key == 46) || (key >= 35 && key <= 40) || (key >= 48 && key <= 57) || (key >= 96 && key <= 105))) {
+            e.preventDefault();
+        }
+    }
+});
 
+$('[AlphaNumeric]').keydown(function (e) {
+    if (e.shiftKey == true) {
+        return false;
+    }
+    let keyCode = e.keyCode ? e.keyCode : e.which;
+    let pressedKey = String.fromCharCode(keyCode);
 
-})
+    if (/[^A-Za-z0-9 ]/.test(pressedKey)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+
+});
+
+$('')
