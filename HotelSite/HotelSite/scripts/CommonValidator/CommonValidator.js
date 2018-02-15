99fe3308d3f186df4cee61ee02507d46acccb43c
Vishi234 <33386171+Vishi234@users.noreply.github.com>
2018-02-16 00:54:51 +0530

    Changes

--- HotelSite/HotelSite/scripts/CommonValidator/CommonValidator.js
+++ HotelSite/HotelSite/scripts/CommonValidator/CommonValidator.js
@@ -5,17 +5,19 @@ $('[NumberOnly]').keydown(function (e) {
     }
     else {
         var key = e.keyCode;
-        if (!((key == 8) || (key == 9) || (key == 46) || (key >= 35 && key <= 40) || (key >= 48 && key <= 57) || (key >= 96 && key <= 105))) {
+        if (!((key == 8) || (key == 46) || (key >= 35 && key <= 40) || (key >= 48 && key <= 57) || (key >= 96 && key <= 105))) {
             e.preventDefault();
         }
     }
 });
 
 $('[AlphaNumeric]').keydown(function (e) {
-
+    //if (e.shiftKey == true) {
+    //    return false;
+    //}
     let keyCode = e.keyCode ? e.keyCode : e.which;
     let pressedKey = String.fromCharCode(keyCode);
-    if (/[^A-Za-z0-9\t\b ]/.test(pressedKey)) {
+    if (/[^A-Za-z0-9\b ]/.test(pressedKey)) {
         return false;
     }
     else {
